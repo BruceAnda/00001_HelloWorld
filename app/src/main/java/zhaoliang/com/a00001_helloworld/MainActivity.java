3c6f25d9f4fd5aa75fc0d5306ac069f169502360
zhaoliang <2668645098@qq.com>
2017-03-15 21:49:41 +0800

    添加广告

--- app/src/main/java/zhaoliang/com/a00001_helloworld/MainActivity.java
+++ app/src/main/java/zhaoliang/com/a00001_helloworld/MainActivity.java
@@ -1,7 +1,10 @@
 package zhaoliang.com.a00001_helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 /**
  * <pre>
@@ -16,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
+                .addTestDevice("AC98C820A50B4AD8A2106EDE96FB87D4")  // An example device ID
+                .build();
+        mAdView.loadAd(request);
+        /*AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);*/
     }
 }
