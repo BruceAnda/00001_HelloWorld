bd39c66e5b9cec7667f1c2a6a76f29f9c1288f8f
zhaoliang <2668645098@qq.com>
2017-03-15 18:07:56 +0800

    Android第一个程序

---
+++ app/src/main/java/zhaoliang/com/a00001_helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package zhaoliang.com.a00001_helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * <pre>
+ *     需求：Android第一个项目
+ *     收获：
+ *          1. 学会使用Android Studio创建Android项目
+ * </pre>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
